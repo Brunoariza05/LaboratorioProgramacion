2a005070660dff5be0b26f5de54ec113113e5a75
brunoariza05 <brunoariza2005@gmail.com>
2025-03-31 12:14:41 -0300

    cambio2

--- LaboratorioProgramacion/Program.cs
+++ LaboratorioProgramacion/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("¡Lindo lunes para estar con Visual Studio 2022!");
+        Console.WriteLine("¡Lindo lunes!");
     }
 }
 
